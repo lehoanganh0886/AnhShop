c44d6a04f81883f1a1e66fbef5b08c86d11d511c
Lê Anh <L Anh>
2016-06-22 23:20:10 +0700

    add domain entity

---
+++ AnhShop.Model/Abstract/Auditable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AnhShop.Model.Abstract
+{
+    public abstract class Auditable : IAuditable
+    {
+        public DateTime CreatedDate { set; get; }
+        public String CreatedBy { set; get; }
+        public DateTime UpdatedDate { set; get; }
+        public string UpdatedBy { set; get; }
+        public string MetaKeyword { set; get; }
+        public string MetaDescription { set; get; }
+        public bool Status { set; get; }
+    }
+}
---
+++ AnhShop.Model/Models/PostTag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnhShop.Model.Models
+{
+    [Table("PostTags")]
+    public class PostTag
+    {
+        [Key]
+        public int PostID { set; get; }
+
+        [Key]
+        [Column(TypeName = "varchar")]
+        [MaxLength(50)]
+        public string TagID { set; get; }
+
+        [ForeignKey("PostID")]
+        public virtual Post Post { set; get; }
+
+        [ForeignKey("TagID")]
+        public virtual Tag Tag { set; get; }
+    }
+}
