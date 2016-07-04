293bcda8ebbc920ec25e1c81cbd0bf1afa091551
Lê Anh <L Anh>
2016-07-04 20:51:35 +0700

    add postcategory service

--- AnhShop.Service/PostService.cs
+++ AnhShop.Service/PostService.cs
@@ -1,35 +1,43 @@
 ﻿using AnhShop.Data.Infrastructure;
 using AnhShop.Data.Repositories;
 using AnhShop.Model.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AnhShop.Service
 {
     public interface IPostService
     {
         void Add(Post post);
-        void Update(Post post);
-        void Delete(int id);
-        IEnumerable<Post> GetAll();
-        IEnumerable<Post> GetAllPaging(int page, int pagSize, out int totalRow);
-        Post GetById(int id);
-        IEnumerable<Post> GetAllByTagPaging(string tag, int page, int pageSize, out int totalRow);
-        void SaveChange();
 
+        void Update(Post post);
+
+        void Delete(int id);
+
+        IEnumerable<Post> GetAll();
+
+        IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow);
+
+        IEnumerable<Post> GetAllByCategoryPaging(int categoryId, int page, int pageSize, out int totalRow);
+
+        Post GetById(int id);
+
+        IEnumerable<Post> GetAllByTagPaging(string tag, int page, int pageSize, out int totalRow);
+
+        void SaveChanges();
     }
+
     public class PostService : IPostService
     {
-        IPostRepository _postRepository;
-        IUnitOfWork _unitOfWork;
+        private IPostRepository _postRepository;
+        private IUnitOfWork _unitOfWork;
+
         public PostService(IPostRepository postRepository, IUnitOfWork unitOfWork)
         {
             this._postRepository = postRepository;
             this._unitOfWork = unitOfWork;
         }
+
         public void Add(Post post)
         {
             _postRepository.Add(post);
@@ -42,12 +50,12 @@ namespace AnhShop.Service
 
         public IEnumerable<Post> GetAll()
         {
-            return _postRepository.GetAll(new string[] {"PostCategory"});
+            return _postRepository.GetAll(new string[] { "PostCategory" });
         }
 
         public IEnumerable<Post> GetAllByTagPaging(string tag, int page, int pageSize, out int totalRow)
         {
-            return _postRepository.GetMultiPaging(x => x.Status, out totalRow, page, pageSize);
+            return _postRepository.GetAllByTag(tag, page, pageSize, out totalRow);
         }
 
         public IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow)
@@ -55,12 +63,17 @@ namespace AnhShop.Service
             return _postRepository.GetMultiPaging(x => x.Status, out totalRow, page, pageSize);
         }
 
+        public IEnumerable<Post> GetAllByCategoryPaging(int categoryID, int page, int pageSize, out int totalRow)
+        {
+            return _postRepository.GetMultiPaging(x => x.Status && x.CategoryID == categoryID, out totalRow, page, pageSize, new string[] { "PostCategory" });
+        }
+
         public Post GetById(int id)
         {
             return _postRepository.GetSingleById(id);
         }
 
-        public void SaveChange()
+        public void SaveChanges()
         {
             _unitOfWork.Commit();
         }
